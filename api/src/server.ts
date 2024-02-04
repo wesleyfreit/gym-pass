a21760f5276d1d4fd9e114bd19322599f524b7e5
Richard Freitas <wesleyrichard30@gmail.com>
2024-02-04 17:29:11 -0300

    chore: add environment variables with zod validation

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,8 @@
 import { app } from './app';
+import { env } from './env';
 
-app.listen(
-  {
-    host: '0.0.0.0',
-    port: 3333,
-  },
-  () => {
-    console.log('Server is running on port 3333');
-  },
-);
+const port = env.PORT;
+
+app.listen({ host: '0.0.0.0', port }, () => {
+  console.log(`Server is running on port ${port}`);
+});
