c935f3a2fca651bd3cfa833be8a930806e80f490
Richard Freitas <wesleyrichard30@gmail.com>
2024-02-04 17:17:17 -0300

    style: format code with eslint and prettier

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,11 @@
-import { app } from "./app";
+import { app } from './app';
 
-app.listen({
-  host: '0.0.0.0',
-  port: 3333
-}, () => {
-  console.log('Server is running on port 3333');
-});
+app.listen(
+  {
+    host: '0.0.0.0',
+    port: 3333,
+  },
+  () => {
+    console.log('Server is running on port 3333');
+  },
+);
