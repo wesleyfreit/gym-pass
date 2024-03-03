7da98881a40e24b1f140cea0d30e5df64a83c930
Richard Freitas <wesleyrichard30@gmail.com>
2024-03-02 21:55:39 -0300

    refactor: remove validation folder and ts path mapping

--- api/src/env/index.ts
+++ api/src/env/index.ts
@@ -1,5 +1,10 @@
-import { envSchema } from '@/validation/envSchema';
 import 'dotenv/config';
+import z from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+});
 
 const _env = envSchema.safeParse(process.env);
 
